1e7225dd065112d47cffabbb701c57e7f47238ef
ryan <ryanandrewbaxter@gmail.com>
2021-03-02 09:41:37 -0600

    Returns to Price class, utilizing state

--- src/index.js
+++ src/index.js
@@ -6,68 +6,84 @@ import reportWebVitals from './reportWebVitals';
 import { config } from './constants'
 import axios from 'axios';
 
-// class Price extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isLoaded: false,
-//       stocks: [],
+class Price extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      stocks: [],
+      stockName: '',
 
-//     };
-//   }
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-//   componentDidMount() {
-//     fetch("https://stonks-python-api.herokuapp.com/yfin/aapl")
-//       .then(res => res.json())
-//       .then(
-//         (result) => {
-//           this.setState({
-//             isLoaded: true,
-//             stocks: result
-//           });
-//           console.log(result);
-//         },
-//       )
-//   }
-//   render() {
-//     const { stocks } = this.state;
-//     return (
-//       <div className="code">
-//         <h3>Current Price of </h3>
-//         <h4>{stocks}</h4>
-//       </div>
-//     );
-//   }
-// }
+  handleChange(event) {
+    this.setState({ stockName: event.target.value });
+  }
 
-function Import() {
-  const [stocks, setStocks] = React.useState([]);
-  React.useEffect(() => {
-    axios.get(`https://stonks-python-api.herokuapp.com/yfin/aapl`)
-      .then(res => {
-        const newStocks = res.data;
-        setStocks(newStocks);
-        console.log(newStocks);
-      });
-  }, [])
-  return (
-    <div>
-      <h1>Please display the stock info!</h1>
-      <p>stock date and close price</p>
-      <ul>
-        {stocks.map((stock, index) =>
-          <li key={index}>Date: {stock.Date} Close: {stock.Close}</li>
-        )}
-      </ul>
-    </div>
-  );
+  handleSubmit(event) {
+    alert('A stock was submitted: ' + this.state.stockName);
+    event.preventDefault();
+  }
+
+  componentDidMount() {
+    fetch("https://stonks-python-api.herokuapp.com/yfin/aapl")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            stocks: result
+          });
+          console.log(result);
+        },
+      )
+  }
+  render() {
+    // const { stocks } = this.state;
+    return (
+      <div className="code">
+        <h3>Current Price of stockname</h3>
+        <ul>
+          {this.state.stocks.map((stock, index) =>
+            <li key={index}>Date: {stock.Date} Close: {stock.Close}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
+
+// function Import() {
+//   const [stocks, setStocks] = React.useState([]);
+//   React.useEffect(() => {
+//     axios.get(`https://stonks-python-api.herokuapp.com/yfin/aapl`)
+//       .then(res => {
+//         const newStocks = res.data;
+//         setStocks(newStocks);
+//         console.log(newStocks);
+//       });
+//   }, [])
+//   return (
+//     <div>
+//       <h1>Please display the stock info!</h1>
+//       <p>stock date and close price</p>
+//       <ul>
+//         {stocks.map((stock, index) =>
+//           <li key={index}>Date: {stock.Date} Close: {stock.Close}</li>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// }
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Import />
+    <Price />
   </React.StrictMode>,
   document.getElementById('root')
 );
